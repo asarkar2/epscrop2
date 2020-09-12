eb8e5282ccb70079ea138eb3701d5692d33c69b0
Anjishnu Sarkar <47971691+asarkar2@users.noreply.github.com>
2020-09-12 23:31:17 +0530

    Update epscrop2.pl

--- epscrop2.pl
+++ epscrop2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
-# Thanks to use answer to use pdfcrop instead of gs to get the correct
-# bounding box 
+# Thanks to use answer (Method B) to use pdfcrop instead of gs to get the 
+# correct bounding box 
 # https://tex.stackexchange.com/a/171460/182471
 
 use warnings ;
